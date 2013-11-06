7533853340531dedfc208a3fbb8048e5050b85cc
Andrew Goodnough <agoodno@gmail.com>
2013-11-05 22:55:22 -0600

    Breaks out and loads a misc file

--- init.el
+++ init.el
@@ -1,4 +1,35 @@
+;; "Emacs outshines all other editing software in approximately the
+;; same way that the noonday sun does the stars. It is not just bigger
+;; and brighter; it simply makes everything else vanish."
+;; -Neal Stephenson, "In the Beginning was the Command Line"
+
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(ack bar-cursor bitlbee color-theme-solarized
+			  color-theme-emacs-revert-theme color-theme js2-mode magit org paredit
+			  yasnippet)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+
+;; Set variable to the .emacs.d directory
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+
+;; Adds .emacs.d directory to the load path
+(add-to-list 'load-path dotfiles-dir)
+
+;; Loads customizations
+(require 'misc)
---
+++ misc.el
@@ -0,0 +1,7 @@
+;; my miscellaneous
+
+(require 'bar-cursor)
+(bar-cursor-mode 1)
+
+;; Create a symbol by which this script can be referenced by a require
+(provide 'misc)
