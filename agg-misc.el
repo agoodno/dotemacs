b8a44186eb1a53db9e8e5a70bc9597017eedecab
Andrew Goodnough <andrew.goodnough@wicourts.gov>
2013-11-12 07:27:25 -0600

    Turned off ensime

--- agg-misc.el
+++ agg-misc.el
@@ -149,7 +149,7 @@
 ;; This step causes the ensime-mode to be started whenever
 ;; scala-mode is started for a buffer. You may have to customize this step
 ;; if you're not using the standard scala mode.
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 ;; MINI HOWTO:
 ;; Open .scala file. M-x ensime (once per project)
