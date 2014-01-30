8d47559a569cf843e25d4b717013276b3952a3d4
Andrew Goodnough <agoodno@gmail.com>
2014-01-29 20:03:02 -0600

    Fixes mouse yank/paste doing strange things on mac

--- agg-misc.el
+++ agg-misc.el
@@ -19,9 +19,9 @@
 (setq-default indent-tabs-mode nil)
 (setq x-select-enable-clipboard t
       ;; x-select-enable-primary t ;;causes problems with delete-selection-mode
-      save-interprogram-paste-before-kill t
+      save-interprogram-paste-before-kill nil
       apropos-do-all t
-      mouse-yank-at-point t
+      mouse-yank-at-point nil
       save-place-file (concat user-emacs-directory "places")
       backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                "backups"))))
