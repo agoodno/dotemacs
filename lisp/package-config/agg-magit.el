c506a98a7713fe33c472643d693c72f0f75f2b2d
Andrew Goodnough <agoodnough@primexinc.com>
2017-03-31 09:49:43 -0500

    Moves repository list to a local file

--- lisp/package-config/agg-magit.el
+++ lisp/package-config/agg-magit.el
@@ -1,6 +1,7 @@
 (use-package magit
   :ensure t
   :init
+  (load "~/.magit-projects")
   (customize-set-variable 'magit-display-buffer-function
     (quote magit-display-buffer-fullframe-status-v1))
   (customize-set-variable 'magit-status-sections-hook
@@ -25,42 +26,8 @@
       (("Name" 40 magit-repolist-column-ident nil)
       ("Path" 99 magit-repolist-column-path))))
   (customize-set-variable 'magit-repository-directories
-    (quote
-      (("~/.emacs.d" . 0)
-      ("~/src/customerservice" . 0)
-      ("~/src/customer-service-client" . 0)
-      ("~/src/deviceapi" . 0)
-      ("~/src/mar-accountalertsettingcreator" . 0)
-      ("~/src/mar-alert-measurement-updater" . 0)
-      ("~/src/mar-alert-rule-deleter" . 0)
-      ("~/src/mar-alert-rule-updater" . 0)
-      ("~/src/mar-alert-service-client" . 0)
-      ("~/src/mar-alertservice" . 0)
-      ("~/src/mar-alexa-voice-ui" . 0)
-      ("~/src/mar-api" . 0)
-      ("~/src/mar-firmwareservice" . 0)
-      ("~/src/mar-forecast-service" . 0)
-      ("~/src/mar-forecast-service-client" . 0)
-      ("~/src/mar-hub-chart-export-sender" . 0)
-      ("~/src/mar-hubapi" . 0)
-      ("~/src/mar-ios" . 0)
-      ("~/src/mar-sensorreadingsprocessor" . 0)
-      ("~/src/mar-system-alert-creator" . 0)
-      ("~/src/mar-systemrulecreator" . 0)
-      ("~/src/mar-valuealertprocessor" . 0)
-      ("~/src/mar-webui" . 0)
-      ("~/src/onevueapi" . 0)
-      ("~/src/ov-clockstatusesprocessor" . 0)
-      ("~/src/ov-probereadingsprocessor" . 0)
-      ("~/src/ov-readings-stream" . 0)
-      ("~/src/ov-readingsprocessor" . 0)
-      ("~/src/ov-twilioprocessor" . 0)
-      ("~/src/ov-utilities" . 0)
-      ("~/src/postman-examples" . 0)
-      ("~/src/primex-authentication" . 0)
-      ("~/src/queue-processor" . 0)
-      ("~/src/rails" . 0))))
-      (global-set-key (kbd "C-c g") 'magit-status)
-      (global-set-key (kbd "C-c l") 'magit-list-repositories))
+    magit-projects)
+  (global-set-key (kbd "C-c g") 'magit-status)
+  (global-set-key (kbd "C-c l") 'magit-list-repositories))
 
 (provide 'agg-magit)
