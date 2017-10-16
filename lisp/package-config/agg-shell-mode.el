b0515974f02d40a939dbc58d4a79c47a10dc67be
Andrew Goodnough <agoodno@gmail.com>
2017-10-16 08:03:52 -0500

    Fixes shell mode colors for programs that have color

--- lisp/package-config/agg-shell-mode.el
+++ lisp/package-config/agg-shell-mode.el
@@ -3,21 +3,24 @@
 ;;; Fix junk characters in shell-mode
 ;; Add color to a shell running in emacs 'M-x shell'
 ;;; Shell mode
-(setq ansi-color-names-vector ; better contrast colors
-      ["black" "red4" "green4" "yellow4"
-       "blue3" "magenta4" "cyan4" "white"])
+;; (setq ansi-color-names-vector ; better contrast colors
+;;       ["black" "red4" "green4" "yellow4"
+;;        "blue3" "magenta4" "cyan4" "white"])
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+
+;; Fixes npm commands that attempt to color interactive user prompts
+(add-to-list
+         'comint-preoutput-filter-functions
+         (lambda (output)
+           (replace-regexp-in-string "\033\\[[0-9]+[A-Z]" "" output)))
+
+;; Fixes some bad characters appearing when color prompts are used
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
 ;; #+TODO: TODO(t) STARTED(s) WAITING(w) | DONE(d) CANCELED(c)
 (setq org-todo-keywords
        '((sequence "TODO" "PROGRAMMING" "DESIGNING" "WAITING" "CHECKLIST" "PR" "APPROVED" "|" "MERGED" "DELEGATED" "DONE" "CANCELED")))
 (setq org-log-done nil)
-
-;; Makes the prompt read-only running in emacs 'M-x shell'
-(add-hook 'shell-mode-hook
-     '(lambda () (toggle-truncate-lines 1)))
-(setq comint-prompt-read-only t)
 
 ;; Makes the prompt read-only running in emacs 'M-x shell'
 (add-hook 'shell-mode-hook
