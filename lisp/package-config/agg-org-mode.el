6b03e95258ed7a5c151f8ee4ba14265b39889658
Andy Goodnough <andy.goodnough@wisc.edu>
2018-02-21 08:09:56 -0600

    Adds some new statuses to org mode

--- lisp/package-config/agg-org-mode.el
+++ lisp/package-config/agg-org-mode.el
@@ -5,7 +5,7 @@
 
 ;; #+TODO: TODO(t) STARTED(s) WAITING(w) | DONE(d) CANCELED(c)
 (setq org-todo-keywords
-       '((sequence "TODO" "PROGRAMMING" "DESIGNING" "WAITING" "CHECKLIST" "PR" "APPROVED" "|" "MERGED" "DELEGATED" "DONE" "CANCELED")))
+       '((sequence "IDEA" "TODO" "PLANNING" "DESIGNING" "PROGRAMMING" "WAITING" "TESTING" "CHECKLIST" "MR" "APPROVED" "|" "MERGED" "DELEGATED" "DONE" "CANCELED")))
 (setq org-log-done nil)
 
 (provide 'agg-org-mode)
--- lisp/package-config/agg-shell-mode.el
+++ lisp/package-config/agg-shell-mode.el
@@ -18,11 +18,6 @@
 ;; Fixes some bad characters appearing when color prompts are used
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-;; #+TODO: TODO(t) STARTED(s) WAITING(w) | DONE(d) CANCELED(c)
-(setq org-todo-keywords
-       '((sequence "TODO" "PROGRAMMING" "DESIGNING" "WAITING" "CHECKLIST" "PR" "APPROVED" "|" "MERGED" "DELEGATED" "DONE" "CANCELED")))
-(setq org-log-done nil)
-
 ;; Makes the prompt read-only running in emacs 'M-x shell'
 (add-hook 'shell-mode-hook
      '(lambda () (toggle-truncate-lines 1)))
