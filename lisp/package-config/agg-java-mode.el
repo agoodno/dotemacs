19b7219919ab8b0777392676a74c5afbded3a0ba
Andy Goodnough <andy.goodnough@wisc.edu>
2018-04-13 09:26:12 -0500

    Also sets the java tab width

--- lisp/package-config/agg-java-mode.el
+++ lisp/package-config/agg-java-mode.el
@@ -1,6 +1,7 @@
 ;; Distributed with GNU Emacs
 
 (add-hook 'java-mode-hook (lambda ()
-                            (setq c-basic-offset 4)))
+                            (setq c-basic-offset 4
+                                  tab-width 4)))
 
 (provide 'agg-java-mode)
