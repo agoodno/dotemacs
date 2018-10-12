5ed0035cb44835ac2818b9ec267d3335d468a6da
Andrew Goodnough <agoodno@gmail.com>
2018-10-11 20:51:30 -0500

    Remove old eclim settings from custom, set elsewhere

--- lisp/agg-custom.el
+++ lisp/agg-custom.el
@@ -6,11 +6,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eclim-eclipse-dirs
-   (quote
-    ("/Applications/eclipse" "/usr/lib/eclipse" "/usr/local/lib/eclipse" "/usr/share/eclipse" "/Applications/Eclipse.app/Contents/Eclipse/" "/home/agoodno/opt/eclipse-java-oxygen-R-linux-gtk-x86_64/")))
- '(eclim-executable
-   "/home/agoodno/opt/eclipse-java-oxygen-R-linux-gtk-x86_64/eclim")
  '(org-return-follows-link t)
  '(package-selected-packages
    (quote
