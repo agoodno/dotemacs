7a0899d0688d6901c826cb19c6a798e6f533ff9d
Andy Goodnough <andy.goodnough@wisc.edu>
2018-10-15 08:33:49 -0500

    Add exec-path-from-shell

--- lisp/agg-custom.el
+++ lisp/agg-custom.el
@@ -9,7 +9,7 @@
  '(org-return-follows-link t)
  '(package-selected-packages
    (quote
-    (auto-complete-nxml fuzzy ac-cider ac-html ac-inf-ruby ac-js2 ac-emacs-eclim eclim markdown-preview-eww auto-complete unfill yaml-mode restclient markdown-mode chruby puppet-mode magit-filenotify magit-find-file tidy smex projectile ido-completing-read+ flycheck-clojure flycheck cider yaml clojure-mode f erc-hipchatify js2-mode ido-vertical-mode ido-mode bundler yari magit aggressive-indent html-mode json-mode enh-ruby-mode bar-cursor auto-compile)))
+    (exec-path-from-shell auto-complete-nxml fuzzy ac-cider ac-html ac-inf-ruby ac-js2 ac-emacs-eclim eclim markdown-preview-eww auto-complete unfill yaml-mode restclient markdown-mode chruby puppet-mode magit-filenotify magit-find-file tidy smex projectile ido-completing-read+ flycheck-clojure flycheck cider yaml clojure-mode f erc-hipchatify js2-mode ido-vertical-mode ido-mode bundler yari magit aggressive-indent html-mode json-mode enh-ruby-mode bar-cursor auto-compile)))
  '(safe-local-variable-values
    (quote
     ((scala-indent:use-javadoc-style . t)
