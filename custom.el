6c43e6b87ebd651687fa7c5766aad822014990fa
Andrew Goodnough <agoodno@gmail.com>
2020-12-10 16:14:03 -0600

    Add unfill paragraph support

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(css-indent-offset 2)
+ '(css-indent-offset 2 t)
  '(json-reformat:indent-width 2)
  '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
  '(magit-repolist-columns
